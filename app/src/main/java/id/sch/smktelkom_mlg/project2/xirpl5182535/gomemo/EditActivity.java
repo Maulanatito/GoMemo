3902a5f2cc9648bc20f815afea7b8a42b8663160
Maulana Tito <titomaulana98@gmail.com>
2017-03-30 21:36:23 +0700

    almost

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl5182535/gomemo/EditActivity.java
@@ -0,0 +1,71 @@
+package id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.javahelps.memo.db.DatabaseAccess;
+
+public class EditActivity extends ActionBarActivity {
+    private EditText etText;
+    private Button btnSave;
+    private Button btnCancel;
+    private id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo.Memo memo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        this.etText = (EditText) findViewById(R.id.etText);
+        this.btnSave = (Button) findViewById(R.id.btnSave);
+        this.btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            memo = (id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo.Memo) bundle.get("MEMO");
+            if(memo != null) {
+                this.etText.setText(memo.getText());
+            }
+        }
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveClicked();
+            }
+        });
+
+        this.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCancelClicked();
+            }
+        });
+    }
+
+    public void onSaveClicked() {
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        if(memo == null) {
+            // Add new memo
+            id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo.Memo temp = new id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo.Memo();
+            temp.setText(etText.getText().toString());
+            databaseAccess.save(temp);
+        } else {
+            // Update the memo
+            memo.setText(etText.getText().toString());
+            databaseAccess.update(memo);
+        }
+        databaseAccess.close();
+        this.finish();
+    }
+
+    public void onCancelClicked() {
+        this.finish();
+    }
+}
